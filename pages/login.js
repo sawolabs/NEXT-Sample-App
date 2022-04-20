5a5daa924b9e39a9c88b9841e79d033cf345053f
abhishekcsawo <88535334+abhishekcsawo@users.noreply.github.com>
2022-04-21 00:45:04 +0530

    Following changes: 1. Replaced sawo-component with sawo 2. Now on login, user details are displayed. 3. made mobile responsive.

--- pages/login.js
+++ pages/login.js
@@ -1,36 +1,47 @@
-import Head from 'next/head'
-import React, { useEffect } from "react";
-import SawoLogin from "sawo-react"
-import lstyles from '../styles/login.module.css'
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
+import Sawo from "sawo";
+import lstyles from "../styles/login.module.css";
 function login() {
-    function sawoLoginCallback(payload) {
-        console.log(payload);
-    }
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [payload, setPayload] = useState({});
 
-    const sawoConfig = {
-        onSuccess: (payload) => {
-            console.log(payload)
-            window.location.href = "/";
-        },
-        identifierType: "email", //required, must be one of: 'email', 'phone_number_sms',
-        apiKey: process.env.NEXT_PUBLIC_SAWO_API_KEY, // required, get it from sawo dev.sawolabs.com,
-        containerHeight: "300px", // the login container height, default is 300px
+  useEffect(() => {
+    var config = {
+      containerID: "sawo-container",
+      identifierType: "email", //required, must be one of: 'email', 'phone_number_sms',
+      apiKey: process.env.NEXT_PUBLIC_SAWO_API_KEY, // required, get it from sawo dev.sawolabs.com,
+      onSuccess: (payload) => {
+        console.log("Payload : " + JSON.stringify(payload));
+        setIsUserLoggedIn(true);
+        setPayload(payload);
+      },
     };
+    let sawo = new Sawo(config);
+    sawo.showForm();
+  }, []);
 
-    return (
+  return (
+    <div className={lstyles.container}>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <div className={lstyles.login_container}>
+        <h2 className="title">SAWO NEXT Sample App</h2>
+        <h2 className="title">User Logged In : {isUserLoggedIn.toString()}</h2>
 
-        <div className={lstyles.container}>
-            <Head>
-                <title>Login</title>
-            </Head>
-            <div className={lstyles.login_container}>
-                <h1>Login</h1>
-                <SawoLogin config={sawoConfig} />
-            </div>
-        </div>
-
-
-    );
-};
+        {!isUserLoggedIn ? (
+          <div className={lstyles.formContainer} id="sawo-container"></div>
+        ) : (
+          <div className={lstyles.loggedin}>
+            <h2>User Successful Login</h2>
+            <div>UserId: {payload.user_id}</div>
+            <div>Verification Token: {payload.verification_token}</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
 
 export default login;
